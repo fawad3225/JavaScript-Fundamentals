96a9575b0002792164c6a2b2b218f08d970756e9
Fawad Khan <khanfawad3225@gmail.com>
2026-07-23 21:44:44 +0500

    Update arrow function practice

--- Functions/02_ArrowFunction.js
+++ Functions/02_ArrowFunction.js
@@ -1,13 +1,18 @@
 /*---------------- Arrow function ------------*/
 const subtract = (a, b) => a - b;
-// console.log(subtract(10, 5));
+console.log(subtract(10, 5));
+
+// Simple structure
+const greet = (name) => console.log(`Hello ${name}`);
+greet("Fawad");
+
 
 // Task 1: write an Arrow function name "calculateTotal" that takes two parameters: Price and Quantity. Function should return totalCost by multiplying price and quantity .
 const calculateTotal = (price, quantity) => {
   let totalCost = price * quantity;
   return `Rs. ${totalCost}`;
 };
-// console.log(calculateTotal(200, 6));
+console.log(calculateTotal(200, 6));
 
 // Task 2: write a function name "processedTeaOrder" that takes another function " makesTea" as parameter and calls it with an argument "earl grey". Return the result of calling makesTea
 function makesTea(typeofTea) {
@@ -17,11 +22,9 @@ function processedTeaOrder(teaFunction) {
   return teaFunction("earl grey");
 }
 let order = processedTeaOrder(makesTea);
-// console.log(order);
+console.log(order);
 
 // Task 3: write a function named "createTeaMaker" that returns another function. The returned function should take one parameter 'teaType' and return a message like 'Making Green Tea'.
-
-
 function createTeaMaker() {
     return function(teaType) {
       return `Making ${teaType}`;
@@ -29,3 +32,13 @@ function createTeaMaker() {
 }
 let TeaMaker = createTeaMaker();
 console.log(TeaMaker("Green tea"));
+
+
+/*--------- Nested Function ------*/
+function orderTea(){
+  function confirmOrder(){
+    return "Order Confirmed";
+  }
+  return confirmOrder();
+}
+console.log(orderTea());
