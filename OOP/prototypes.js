4228123af1635b7fcdab06936f378a1b600b0176
Fawad Khan <khanfawad3225@gmail.com>
2026-07-25 00:29:00 +0500

    updated

--- OOP/prototypes.js
+++ OOP/prototypes.js
@@ -1,9 +1,19 @@
+/*---------Ways of decalaring prototype ---------*/
+
+// Type 1
 let computer = { Cpu: 12 };
 let Lenovo = {
-    screen : "HD",
-    __proto__: computer
-}
-console.log(Lenovo.__proto__);
-console.log(Lenovo.Cpu);
-console.log(Lenovo);
+  screen: "HD",
+  __proto__: computer,
+};
+// console.log(Lenovo.__proto__);
+// console.log(Lenovo.Cpu);
+// console.log(Lenovo);
 
+// Type 2
+let car = { tyres: 4 };
+let tesla = {
+  driver: "AI",
+};
+Object.setPrototypeOf(tesla, car);
+console.log(Object.getPrototypeOf(tesla));
